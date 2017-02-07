2cd12af642428aaccbfcafc3df31a21e28c74030
Jacob Gold <goldjacob29@uchicago.edu>
2017-02-06 22:25:09 -0600

    Director and actor done

--- app/controllers/msm_controller.rb
+++ app/controllers/msm_controller.rb
@@ -25,15 +25,99 @@ class MsmController < ApplicationController
   end
 
   def director_add
-    render("/msm/directors/new_form.html.erb")
+    render("/msm/directors/director_new_form.html.erb")
 
   end
+
+  def director_create
+    d = Director.new
+    d.name = params[:name]
+    d.bio = params[:bio]
+    d.dob = params[:dob]
+    d.image_url = params[:image_url]
+    d.save
+
+    redirect_to("/directors/#{d.id}")
+  end
+
+  def director_edit
+
+    @d = Director.find_by({:id => params[:id]})
+
+    render("/msm/directors/director_edit.html.erb")
+
+  end
+
+  def director_update
+
+    d = Director.find_by({:id => params[:id]})
+    d.name = params[:name]
+    d.bio = params[:bio]
+    d.dob = params[:dob]
+    d.image_url = params[:image_url]
+    d.save
+
+    redirect_to("/directors/#{d.id}")
+
+  end
+
+
+
 
   # actors
 
   def actor_index
 
+    @list_of_actors = Actor.all
     render("/msm/actors/actor_index.html.erb")
+
+  end
+
+  def actor_show
+    @a = Actor.find_by({:id => params[:id]})
+    render("/msm/actors/actor_show.html.erb")
+  end
+
+  def actor_delete
+    a = Actor.find_by({:id => params[:id]})
+    a.destroy
+    redirect_to("/actors")
+  end
+
+  def actor_add
+    render("/msm/actors/actor_new_form.html.erb")
+
+  end
+
+  def actor_create
+    a = Actor.new
+    a.name = params[:name]
+    a.bio = params[:bio]
+    a.dob = params[:dob]
+    a.image_url = params[:image_url]
+    a.save
+
+    redirect_to("/actors/#{a.id}")
+  end
+
+  def actor_edit
+
+    @a = Actor.find_by({:id => params[:id]})
+
+    render("/msm/actors/actor_edit.html.erb")
+
+  end
+
+  def actor_update
+
+    a = Actor.find_by({:id => params[:id]})
+    a.name = params[:name]
+    a.bio = params[:bio]
+    a.dob = params[:dob]
+    a.image_url = params[:image_url]
+    a.save
+
+    redirect_to("/actors/#{a.id}")
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
 
   # Directors
 
-  get("/directors/new_form", { :controller => "msm", :action => "director_add"})
+  get("/directors/director_new_form", { :controller => "msm", :action => "director_add"})
+  get("/directors/director_create", { :controller => "msm", :action => "director_create"})
+
+  get("/directors/:id/edit_form", { :controller => "msm", :action => "director_edit"})
+  get("/directors/director_update/:id", { :controller => "msm", :action => "director_update"})
 
   get("/directors", { :controller => "msm", :action => "director_index" })
   get("/directors/:id", { :controller => "msm", :action => "director_show" })
@@ -16,6 +20,17 @@ Rails.application.routes.draw do
   # Actors
 
   get("/actors", { :controller => "msm", :action => "actor_index" })
+
+  get("/actors/actor_new_form", { :controller => "msm", :action => "actor_add"})
+  get("/actors/actor_create", { :controller => "msm", :action => "actor_create"})
+
+  get("/actors/:id/edit_form", { :controller => "msm", :action => "actor_edit"})
+  get("/actors/actor_update/:id", { :controller => "msm", :action => "actor_update"})
+
+  get("/actors", { :controller => "msm", :action => "actor_index" })
+  get("/actors/:id", { :controller => "msm", :action => "actor_show" })
+
+  get("/delete_actor/:id", { :controller => "msm", :action => "actor_delete"})
 
   # Movies
 
