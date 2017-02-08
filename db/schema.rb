5db9e5176c0bb76d8e7f62aa1649f1d192fadd89
Jacob Gold <goldjacob29@uchicago.edu>
2017-02-08 15:04:48 -0600

    d

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207023837) do
+ActiveRecord::Schema.define(version: 20170207223607) do
 
   create_table "actors", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(version: 20170207023837) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string   "source"
+    t.text     "caption"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
